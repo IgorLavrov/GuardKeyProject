54f668ed50e585e7aa8d97e95f07395ea5a1d5a6
IrinaTln <ikosenkova@gmail.com>
2024-02-14 10:22:43 +0200

    Fixed image bugs

--- GuardKeyProject/GuardKeyProject/Views/MainPage.xaml.cs
+++ GuardKeyProject/GuardKeyProject/Views/MainPage.xaml.cs
@@ -22,16 +22,15 @@ namespace GuardKeyProject.Views
 
             var absoluteLayout = new AbsoluteLayout();
 
-            // Background Image
-            //var backgroundImage = new Image
-            //{
-            //    Source = ".jpg", // replace with your image source
-            //    Aspect = Aspect.AspectFill,
-            //    Opacity = 0.20
-            //};
-            //AbsoluteLayout.SetLayoutFlags(backgroundImage, AbsoluteLayoutFlags.All);
-            //AbsoluteLayout.SetLayoutBounds(backgroundImage, new Rectangle(0, 0, 1, 1));
-            //absoluteLayout.Children.Add(backgroundImage);
+            
+            var backgroundImage = new Image
+            {
+                Source = "background.png", // replace with your image source
+                Aspect = Aspect.AspectFill
+            };
+            AbsoluteLayout.SetLayoutFlags(backgroundImage, AbsoluteLayoutFlags.All);
+            AbsoluteLayout.SetLayoutBounds(backgroundImage, new Rectangle(0, 0, 1, 1));
+            absoluteLayout.Children.Add(backgroundImage);
 
             Button button = new Button();
             button.Text = "Add Cart";
